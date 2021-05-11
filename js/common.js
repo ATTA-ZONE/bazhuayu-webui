138ab89cb689e10959ba5db81ee46da6692ab91c
mouren <1832819123@qq.com>
2021-05-11 11:50:11 +0800

    test video fobid

--- js/common.js
+++ js/common.js
@@ -26,7 +26,7 @@ $.ajax({
 $(document).ready(function(){
 	var videoMask = '<div style="position:absolute;top:0;left:0;width:100%;height:100%;z-index:10;"><div>';
 	$('video').parent().css({
-		position: relative
+		'position': 'relative'
 	})
 	$('video').parent().append(videoMask)
 });
