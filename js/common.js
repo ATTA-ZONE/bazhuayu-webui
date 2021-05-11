b50dd74a9976db85d20923bfd87e22571a3d417d
mouren <1832819123@qq.com>
2021-05-11 11:48:42 +0800

    test video fobid

--- js/common.js
+++ js/common.js
@@ -24,7 +24,11 @@ $.ajax({
 });
 
 $(document).ready(function(){
-	$('video').bind('contextmenu',function() { return false; });
+	var videoMask = '<div style="position:absolute;top:0;left:0;width:100%;height:100%;z-index:10;"><div>';
+	$('video').parent().css({
+		position: relative
+	})
+	$('video').parent().append(videoMask)
 });
 
 // $.ajax({
