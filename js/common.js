a80d55f97be8eb461354b4e8f6b969f1b4104df6
wangjinyi <wangjinyi143>
2021-05-13 15:02:08 +0800

    5.13-5-test

--- js/common.js
+++ js/common.js
@@ -151,7 +151,7 @@ $(function(){
 		$('.tc-show').removeClass('modify-tc-pc');
 		$('.tc-show').addClass('modify-tc-mobile');
 		
-		$('.tc-show.modify-tc-mobile').on('click',function(){
+		$('.accountclass.modify-tc-mobile').on('click',function(){
 			$('.modify').addClass('modify-tc-active')
 		})
 	}
