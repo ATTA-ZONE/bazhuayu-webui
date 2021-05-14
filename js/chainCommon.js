5f3baa3e6cf96d916359ff0c4252f0947270d143
mouren <1832819123@qq.com>
2021-05-14 10:31:33 +0800

    no message

--- js/chainCommon.js
+++ js/chainCommon.js
@@ -22,20 +22,22 @@ function accountsChanged(fnc) {
 }
 
 function changeNetwork(status) {
-  window.ethereum.request({
-    method: 'wallet_addEthereumChain',
-    params: [{
-      chainId: RPCSetting[status].CHAIN_ID_HEX,
-      chainName: RPCSetting[status].CHAIN_NAME,
-      nativeCurrency: {
-        name: RPCSetting[status].SYMBOL,
-        symbol: RPCSetting[status].SYMBOL,
-        decimals: 18
-      },
-      rpcUrls: RPCSetting[status].RPC_URL,
-      blockExplorerUrls: RPCSetting[status].ETHERSCAN_URL
-    }]
-  });
+  return new Promise(()=>{
+    window.ethereum.request({
+      method: 'wallet_addEthereumChain',
+      params: [{
+        chainId: RPCSetting[status].CHAIN_ID_HEX,
+        chainName: RPCSetting[status].CHAIN_NAME,
+        nativeCurrency: {
+          name: RPCSetting[status].SYMBOL,
+          symbol: RPCSetting[status].SYMBOL,
+          decimals: 18
+        },
+        rpcUrls: RPCSetting[status].RPC_URL,
+        blockExplorerUrls: RPCSetting[status].ETHERSCAN_URL
+      }]
+    });
+  })
 }
 
 // 监听网络变更事件
