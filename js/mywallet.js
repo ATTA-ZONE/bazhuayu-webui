6210a195f2cd7536ecdbfb9ec9202590634f918d
mouren <1832819123@qq.com>
2021-05-14 11:54:08 +0800

    no message

--- js/mywallet.js
+++ js/mywallet.js
@@ -11,9 +11,9 @@ function mangeWalletCharge(res, accounts) {
 		var num = getWeb3().utils.toWei(amount, 'ether');
 		debugger
 		contract.methods.balanceOf(accounts[0]).call() //查询余额
-			.then(function (res) {
+			.then(function (resBalance) {
 
-				if (Number(res) >= Number(num)) {
+				if (Number(resBalance) >= Number(num)) {
 					setTimeout(function () {
 						contract.methods.transfer(cwallet, num).send({ //转账
 								from: accounts[0]
