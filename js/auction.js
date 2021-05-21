815b6af2482dba2a8c4a03d56cdb2e2ee094dfa5
wangjinyi <wangjinyi143>
2021-05-21 11:47:50 +0800

    test-4

--- js/auction.js
+++ js/auction.js
@@ -393,7 +393,7 @@ if (typeof window.ethereum !== 'undefined') {
 	accountsChangedAssign(accountsChangedImplement);
 
 } else {
-	if (getCookie('isConnect') == 'false') {
+	if (getCookie('isConnect') == false) {
 		$('#make_offer').data('sign', '4');
 		var html = `<div>請先安裝MetaMask，以保證拍賣功能的使用</div>
 					<a style="font-size:16px; display:block; color:#9567FF; margin-top:5px;" href="https://metamask.io/">轉到MetaMask的網站</a>`;
