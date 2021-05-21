2db0e5ab80f9c262945d1333896f70cdbbd9fa95
wangjinyi <wangjinyi143>
2021-05-21 11:17:16 +0800

    5.21-3

--- js/common.js
+++ js/common.js
@@ -226,7 +226,6 @@ $(function(){
 	$.ajax({
 		url:base_url+'/v2/user/wallet/info',
 		success:function(res){
-			debugger
 			console.log("islogin-----"+islogin);
 			if(res.code==0){
 				$('.header-right-wallet').show();
