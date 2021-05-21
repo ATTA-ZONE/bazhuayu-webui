649bf9d763815e902ce8425c705441054082eba0
wangjinyi <wangjinyi143>
2021-05-21 11:19:28 +0800

    5.21-6

--- js/common.js
+++ js/common.js
@@ -7,6 +7,12 @@ url = url.substring(0,url.indexOf('.'));
 // var base_url = 'http://47.118.74.48:8081';
 // var base_url = 'http://58.212.110.92:8866';
 var base_url = '';
+var islogin;
+if (getCookie('islogin') != 'false') {
+	islogin = getCookie('islogin');
+}else{
+	islogin = false;
+}
 if (window.location.href.indexOf('bazhuayu.io') == -1) {
 	base_url = 'http://localhost:8081';
 	if (window.location.href.indexOf('47.118.74.48:') > -1) {
