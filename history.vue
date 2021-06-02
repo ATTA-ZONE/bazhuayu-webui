6f448dde490675be3fd95a61e9a5f50525bb325c
zyh <1832819123@qq.com>
2021-06-02 15:09:18 +0800

    update

--- history.vue
+++ history.vue
@@ -3,10 +3,7 @@
     <div class="filter-wrap">
       <div class="filter-control" @click="toggleFilters">
         <span>全部</span>
-        <img
-          :class="showFilters ? 'roate' : ''"
-          src="./images/selectMore.png"
-        />
+        <img :class="showFilters ? 'roate' : ''" src="./images/selectMore.png" />
       </div>
       <ul v-if="showFilters" class="filter-items">
         <li>鑄造記錄</li>
@@ -15,11 +12,7 @@
       </ul>
     </div>
     <div class="history-items">
-      <div
-        class="history-item"
-        v-for="(item, index) in historyData.mintRecords"
-        :key="index"
-      >
+      <div class="history-item" v-for="(item, index) in historyData.mintRecords" :key="index">
         <div class="history-title">
           <div class="title-info">
             <span>LOG{{ index }}</span>
@@ -34,7 +27,10 @@
           </div>
           <div class="desc-info">
             <div>接收地址由： 0xC2C747E0F7004F9E8817Db2ca4997657a7746928</div>
-            <div>更改為：<span class="desc-info-address">0xf2102117f279d9c30ffa4149d6670d349cb721af</span> </div>
+            <div>
+              更改為：
+              <span class="desc-info-address">0xf2102117f279d9c30ffa4149d6670d349cb721af</span>
+            </div>
           </div>
         </div>
       </div>
@@ -42,49 +38,65 @@
   </div>
 </template>
 <script>
-  module.exports =  {
-    data() {
-      return {
-        showFilters: false,
-        historyData: {},
-      };
+module.exports = {
+  data() {
+    return {
+      showFilters: false,
+      historyData: {},
+    };
+  },
+  created() {
+    this.getHistory();
+  },
+  mounted(){
+    this.getNftHistory()
+  },
+  methods: {
+    toggleFilters() {
+      this.showFilters = !this.showFilters;
     },
-    created() {
-      //this.getHistory();
-      //this.getNftHistory()
-    },
-    methods: {
-      toggleFilters() {
-        this.showFilters = !this.showFilters;
-      },
-      getHistory() {
-        let self = this;
-        $.ajax({
-          url: base_url + "/v2/user/nft/records",
-          success: function (res) {
-            if (res.code == 0) {
-              self.historyData = res.data;
-            }
-          },
-        });
-      },
-      getNftHistory(){
-        let self = this
-        var web3 = new Web3(CHAIN.WALLET.provider());
-        var chainId = '';
-        CHAIN.WALLET.chainId().then(function (res) {
-          chainId = web3.utils.hexToNumber(res);    
-          if (chainId != targetChainId) {
-            CHAIN.WALLET.switchRPCSettings(targetChainId);
+    getHistory() {
+      let self = this;
+      $.ajax({
+        url: base_url + "/v2/user/nft/records",
+        success: function(res) {
+          if (res.code == 0) {
+            self.historyData = res.data;
           }
-          auctionAddress = contractSetting['atta_ERC721'][chainId].address;
-          $.ajax({
-            url: scansite_base_url + '/api?module=account&action=tokennfttx&contractaddress=' + auctionAddress + '&address='+ walletId +'&sort=asc'
-          })
-        }
-      }
+        },
+      });
     },
-  };
+    getNftHistory() {
+      let self = this
+      var chainId = '';
+      var targetChainId = '';
+      var scansite_apiKey = '';
+      var scansite_base_url = '';
+      
+      if (window.location.href.indexOf('bazhuayu.io') == -1) {
+        targetChainId = 97;
+        scansite_apiKey = ''
+        scansite_base_url = 'https://api-testnet.bscscan.com'
+      } else {
+        targetChainId = 56;
+        scansite_apiKey = '9GRF9Q9HT18PBCHQQD84N7U2MGC6I1NE27';
+        scansite_base_url = 'https://api.bscscan.com'
+      }
+      console.log(window);
+      var web3 = new Web3(window.CHAIN.WALLET.provider());
+      window.CHAIN.WALLET.chainId().then(function(res) {
+        chainId = web3.utils.hexToNumber(res);
+        if (chainId != targetChainId) {
+          window.CHAIN.WALLET.switchRPCSettings(targetChainId);
+        }
+        auctionAddress = contractSetting['atta_ERC721'][chainId].address;
+        $.ajax({
+          url: scansite_base_url + '/api?module=account&action=tokennfttx&contractaddress=' + auctionAddress + '&address=' + walletId + '&sort=asc'
+        })
+      })
+    }
+  },
+};
 </script>
 <style>
 .history-items {
