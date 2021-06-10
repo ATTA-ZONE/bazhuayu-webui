94bd2a530bd0697a2c874efae104948c201a3815
wangjinyi <wangjinyi143>
2021-06-10 15:59:24 +0800

    增加複製地址功能

--- js/mywallet.js
+++ js/mywallet.js
@@ -266,3 +266,17 @@ function  panduan() {
 		window.location.search = "";
 	}
 }
+function copyaddressbtn() {
+	let val = $("#testcopy").text();
+	let transfer = document.createElement('input');
+    document.body.appendChild(transfer);
+    transfer.value = val.split(" ")[1];  // 这里表示想要复制的内容
+    transfer.focus();
+    transfer.select();
+    if (document.execCommand('copy')) {
+        document.execCommand('copy');
+    }
+    transfer.blur();
+	tips("複製成功");
+    document.body.removeChild(transfer);
+}
