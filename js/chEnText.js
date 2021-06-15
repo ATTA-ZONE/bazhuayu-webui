c766bbc13534a3dc5739e404a7cef8fd6886e035
wanglan <809382327@qq.com>
2021-06-15 15:38:14 +0800

    中英文集合

--- js/chEnText.js
+++ js/chEnText.js
@@ -130,21 +130,21 @@ var chEnText = {
       "walletLose":"錢包連接已失效，請重新連接錢包"
     },
     "EN":{
-      "haveBid":"我的出價",
-      "bid":"報價不能低於初始出價",
-      "startingPrice":"拍賣開始時間：",
-      "endPrice":"拍賣剩餘時間：",
+      "haveBid":"My offer",
+      "bid":"The offer cannot be lower than the initial offer",
+      "startingPrice":"Bidding starts in：",
+      "endPrice":"Bidding ends in：",
       "auctionEnd":"競標結束",
-      "topPrice":"恭喜！ 您是出價最高者！",
-      "assetLook":"去我的資產查看",
-      "bidFailure":"您上次競標失敗",
-      "lose":"未登入/錢包連接已失效",
-      "notBegun":"拍賣未開始",
-      "msg01":"請先安裝MetaMask/或者使用WalletConnect，以保證拍賣功能的使用",
-      "msg02":"轉到MetaMask的網站",
-      "address01":"當前連接錢包地址為：",
-      "address02":"當前頁面的競拍記錄以當前連接的錢包為準，如果您使用了其他錢包參與競拍，可以切換至其他錢包查看競拍記錄。",
-      "walletLose":"錢包連接已失效，請重新連接錢包"
+      "topPrice":"Congrats! You are the finnal winner!",
+      "assetLook":"Go to my asset to check",
+      "bidFailure":"Your bid has been exceeded ",
+      "lose":"Not logged in/Unconnected wallet",
+      "notBegun":"AUCTION Not started",
+      "msg01":"Please install metamask first to ensure the use of audit function",
+      "msg02":"Go to metamask's website",
+      "address01":"Current wallet address：",
+      "address02":"The bidding records shown are attatched to current wallet. You can switch to other wallet if you want to check the bidding records related to other wallet.",
+      "walletLose":"The wallet connection has failed. Please connect the wallet again"
     }
   },
   "auctionPayment":{
@@ -166,21 +166,21 @@ var chEnText = {
       "payAddress":"付費地址："
     },
     "EN":{
-      "walletLose":"錢包連接已失效，請重新連接錢包",
-      "hintMessage0101":"您的賬戶未綁定地址，點擊確定會為您自動綁定當前錢包地址\n基於區塊鏈的獨立性，即使不綁定您仍然可以參與競拍\n當前錢包地址: ",
-      "hintMessage0102":" \n賬戶綁定地址: null",
-      "hintMessage0201":"您的賬戶綁定地址與當前錢包地址不符，點擊確定會為您重新綁定當前錢包地址\n基於區塊鏈的獨立性，即使不綁定您仍然可以參與競拍\n當前錢包地址: ",
-      "hintMessage0202":" \n賬戶綁定地址: ",
-      "bindSuc":"綁定成功！",
-      "bindErr":"綁定失敗！",
-      "noLogin":"您尚未登錄，無法綁定錢包，但基於區塊鏈的獨立性，您仍然可以參與競拍\n當前錢包地址: ",
-      "priceSuc":"競價成功",
-      "priceErr":"競價失敗！",
-      "accomplish":"完成",
-      "youPriceSuc":"您的競標成功。",
-      "number":"訂單號：",
-      "youPrice":"您的競標價格為",
-      "payAddress":"付費地址："
+      "walletLose":"Wallet disconnected, please connect again",
+      "hintMessage0101":"Your account is not bound to address. Click OK to bind the current wallet address automatically for you\nDue to the independence of the blockchain, you can still participate in the auction even if it is not bound\nCurrent wallet address: ",
+      "hintMessage0102":" \nAccount binding address: null",
+      "hintMessage0201":"Your account binding address does not match the current wallet address. Click OK to rebind the current wallet address for you\nDue to the independence of the blockchain, you can still participate in the auction even if it is not bound\nCurrent wallet address: ",
+      "hintMessage0202":" \nAccount binding address: ",
+      "bindSuc":"Binding succeeded!",
+      "bindErr":"Binding failed!",
+      "noLogin":"You have not signed in yet and cannot bind the wallet. However, due to the independence of the blockchain, you can still participate in bidding for the current wallet address: ",
+      "priceSuc":"Successful bidding",
+      "priceErr":"Bid failed！",
+      "accomplish":"complete",
+      "youPriceSuc":"Your bid is successful.",
+      "number":"Order No：",
+      "youPrice":"Your bid price is",
+      "payAddress":"Billing address:"
     } 
   },
   "common":{
@@ -198,17 +198,17 @@ var chEnText = {
       "noConnectWallet":"未連接錢包"
     },
     "EN":{
-      "logOutMes":"你確定要登出嗎",
-      "suc":"成功",
-      "cancel":"取消",
-      "myaccount":"我的帳戶",
-      "myorders":"我的訂單",
-      "myassets":"我的資產",
-      "mywallet":"我的錢包",
-      "logOut":"登出",
-      "login":"登入/註冊",
-      "connectWallet":"已連接錢包",
-      "noConnectWallet":"未連接錢包"
+      "logOutMes":"Are you sure you want to log out",
+      "suc":"success",
+      "cancel":"cancel",
+      "myaccount":"My Account",
+      "myorders":"My Orders",
+      "myassets":"My Assets",
+      "mywallet":"My Wallet",
+      "logOut":"Log out",
+      "login":"Login/Sign up",
+      "connectWallet":"Wallet connected",
+      "noConnectWallet":"Connect Wallet"
     }
   },
   "connectWallet":{
@@ -228,19 +228,19 @@ var chEnText = {
       "alert":"請使用任意錢包Dapp中自帶的瀏覽器訪問 bazhuayu.io，則可成功連接錢包。或請使用電腦，通過瀏覽器的錢包插件連接錢包。"
     },
     "EN":{
-      "logWtllet":"您成功登記了一個錢包",
-      "connectWallet":"請連接錢包",
-      "walletAddress":"錢包地址：",
-      "tips01":"請註意：您購買的NFT資產只會發放至當前登記的錢包",
-      "tips02":"（ 如果您想使用其他錢包，請點擊右上角“已連接錢包”進行更換 ）",
-      "know":"知道了",
+      "logWtllet":"You successfully registered a wallet",
+      "connectWallet":"Please connect your wallet",
+      "walletAddress":"Wallet address：",
+      "tips01":"Note: The NFT assets you purchased will be sent to the current wallet you connected only.",
+      "tips02":"（ Please click the 'wallet connected' button to change to another wallet if you need. ）",
+      "know":"Got it!",
       "changeWallet":"該錢包已被綁定占有, 請更換錢包",
       "emailErr":"郵箱未驗證",
-      "loginNot":"登錄已失效，請重新登錄",
+      "loginNot":"Please log in first.",
       "windowErr":"系統錯誤",
-      "httpError":"網絡錯誤，無法獲取賬戶信息",
-      "unableGet":"無法獲取錢包信息",
-      "alert":"請使用任意錢包Dapp中自帶的瀏覽器訪問 bazhuayu.io，則可成功連接錢包。或請使用電腦，通過瀏覽器的錢包插件連接錢包。"
+      "httpError":"Error, cannot receive account info.",
+      "unableGet":"Cannot receive wallet info.",
+      "alert":"In order to connect your wallet successfully, please use the built-in browser within any wallet Dapp to visit BAZHUAYU.IO, or use the wallet add-ons of the PC browser on desktop PC."
     }
   },
   "forgotPassword":{
@@ -261,10 +261,10 @@ var chEnText = {
       "validCvv":"請輸入有效的cvv程式碼"
     },
     "EN":{
-      "number":"卡號",
-      "validCardNumber":"請輸入有效的卡號",
-      "validDate":"請輸入有效的到期日期",
-      "validCvv":"請輸入有效的cvv程式碼"
+      "number":"card number",
+      "validCardNumber":"Please enter a valid card number",
+      "validDate":"Please enter a valid expiry date",
+      "validCvv":"Please enter a valid cvv code"
     }
   },
   "function":{
@@ -311,46 +311,46 @@ var chEnText = {
       "noLog":"未登錄，請登入"
     },
     "EN":{
-      "passWord":"密碼至少8位，請至少包含一個大寫字母和一個小寫字母",
-      "recharge":"充值",
-      "nowRecharge":"立即充值",
-      "connectWallet":"請連接錢包",
-      "amount":"輸入金額",
-      "withdraw":"提款",
-      "nowWithdraw":"立即提款",
-      "changeCard":"更換信用卡",
-      "deleteData":"刪除資料",
-      "deleteCard":"您確定要刪除信用卡信息嗎？不能撤消。",
-      "delete":"刪除",
-      "deleteWallt":"您確定要刪除錢包信息嗎？不能撤消。",
-      "chengeNick":"更換暱稱",
-      "currentNick":"當前暱稱：",
-      "newNick":"輸入新暱稱",
-      "save":"儲存",
-      "changeNFT":"修改BSC NFT接收地址",
-      "currentAddress":"當前接收地址：",
-      "noAddress":"暫無接收地址",
-      "newAddress":"新接收地址：",
-      "tips01":"提示：一旦鑄造完成，則不可修改地址。如需查看該NFT，需連接新接收地址才可查看",
-      "edit":"確認修改",
-      "transfer":"轉移Token",
-      "newWallt":"至新錢包",
-      "walltAdress":"當前所在錢包地址：",
-      "transferTo":"轉移至：",
-      "tips02":"提示：轉移至新錢包后，需連接新錢包才可在“我的NFT”中查看",
-      "confirmCurrent":"確認轉移",
-      "changePassword":"更換密碼",
-      "currentPassword":"當前密碼",
-      "enterCurrentPassword":"輸入當前密碼",
-      "currentPasswordErr":"當前密碼不正確。",
-      "newPassword":"新密碼",
-      "entweNewPassword":"輸入新密碼",
+      "passWord":"At least 8 digits, must contain one upper case letter and one lower case letter",
+      "recharge":"Add funds",
+      "nowRecharge":"Withdraw now",
+      "connectWallet":"Place connect wallet",
+      "amount":"Enter amount",
+      "withdraw":"Withdraw",
+      "nowWithdraw":"Withdraw now",
+      "changeCard":"Change credit card",
+      "deleteData":"Delete credit card",
+      "deleteCard":"Are you sure you want to delete credit card information? Cannot be undone.",
+      "delete":"Delete",
+      "deleteWallt":"Are you sure you want to delete wallet information? Cannot be revoked.",
+      "chengeNick":"Change nickname",
+      "currentNick":"Current nickname",
+      "newNick":"Enter your new nickname",
+      "save":"Submit",
+      "changeNFT":"Edit Receiving Address",
+      "currentAddress":"Current address：",
+      "noAddress":"No receiving address",
+      "newAddress":"New address：",
+      "tips01":"Note: You cannot change the receiving address once minting is completed. You need to connect to the new address (wallet) if you want to check the NFT.",
+      "edit":"Confirm",
+      "transfer":"Transfer Token ",
+      "newWallt":" to new wallet",
+      "walltAdress":"Current addr：",
+      "transferTo":"Transfer to：",
+      "tips02":"Note: After you transfered your NFT, you need to connect the wallet you transfered to, to check your NFTs’status",
+      "confirmCurrent":"Confirm",
+      "changePassword":"Change password",
+      "currentPassword":"Current password",
+      "enterCurrentPassword":"Enter current password",
+      "currentPasswordErr":"The current password you entered is not correct",
+      "newPassword":"New password",
+      "entweNewPassword":"Enter new password",
       "passwordLength":"密碼長度不少於6比特",
-      "passwordReg":"密碼至少8位，請至少包含一個大寫字母和一個小寫字母",
-      "repeatPassword":"重複新密碼",
-      "repeatPasswordErr":"重複密碼不正確",
-      "noConnectWallet":"未連接錢包",
-      "noLog":"未登錄，請登入"
+      "passwordReg":"At least 8 digits, must contain one upper case letter and one lower case letter",
+      "repeatPassword":"Enter new password again",
+      "repeatPasswordErr":"The password you entered Doesn't match your new password",
+      "noConnectWallet":"Connect Wallet",
+      "noLog":"Not logged in, please log in"
     }
   },
   "login":{
@@ -388,7 +388,7 @@ var chEnText = {
       "tables":["我的藏品","我的NFT","NFT操作記錄"]
     },
     "EN":{
-      "tables":["我的藏品","我的NFT","NFT操作記錄"]
+      "tables":["My Collection","My NFT","NFT Logs"]
     }
   },
   "mywalletCard":{
@@ -399,10 +399,10 @@ var chEnText = {
       "validCvv":"請輸入有效的cvv程式碼"
     },
     "EN":{
-      "number":"卡號",
-      "validCardNumber":"請輸入有效的卡號",
-      "validDate":"請輸入有效的到期日期",
-      "validCvv":"請輸入有效的cvv程式碼"
+      "number":"card number",
+      "validCardNumber":"Please enter a valid card number",
+      "validDate":"Please enter a valid expiry date",
+      "validCvv":"Please enter a valid cvv code"
     } 
   },
   "reset":{
@@ -429,12 +429,12 @@ var chEnText = {
       "deleteSuc":"删除成功"
     },
     "EN":{
-      "bindWallt":"當前綁定錢包地址如下：",
-      "NFTWallt":"當前購買的所有NFT資產將會默認發送至以上錢包地址",
-      "change":"更換",
-      "delete":"刪除",
-      "cancel":"取消",
-      "deleteSuc":"删除成功"
+      "bindWallt":"Current wallet address：",
+      "NFTWallt":"All NFT assets you purchased will be sent to the address above.",
+      "change":"Change",
+      "delete":"Delete",
+      "cancel":"Cancel",
+      "deleteSuc":"delete sucessful"
     }
   },
   "signup":{
