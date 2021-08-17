4568c7a6eb28c8299b2e93cf37937cc676b67384
wangjinyi <wangjinyi143>
2021-08-17 13:19:32 +0800

    测试完成

--- blindbox.vue
+++ blindbox.vue
@@ -673,8 +673,8 @@ module.exports = {
     toPay(str) {
       var self = this;
       var now = new Date();
-      // var startnow = new Date('2021/8/12 20:00');
-      var startnow = new Date('2021/8/19 20:00');
+      var startnow = new Date('2021/8/12 20:00');
+      // var startnow = new Date('2021/8/19 20:00');
       var endDate = new Date("2021/8/20 12:00");
       if (startnow.getTime() > now.getTime()) {
         tips(self.chEnTextHtml[self.lang].activitytips1);
