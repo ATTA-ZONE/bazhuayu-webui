e227a8ccdb72e5e635cc18c3db0f3fff588e8542
wanglan <809382327@qq.com>
2021-08-31 17:40:31 +0800

    本地测试

--- js/artwork.js
+++ js/artwork.js
@@ -73,16 +73,16 @@ function getArtworkList(current,pageSize,name,typeId){
 						}
 						
 						if(geshi=='mp4'){
-							if(v.secondPic){
-								html +=  `<li><a class="artwork-mask" href="${v.releaseType == 2 ? 'auctionDetails.html?id='+v.id : 'artworkDetails.html?id='+v.id}"><div class="artwork-mask-wrap"></div>`;
-								html+=`<img class="bzy-e-list-img" src="`+v.secondPic+`" >`;
-							}else{
+							// if(v.secondPic){
+							// 	html +=  `<li><a class="artwork-mask" href="${v.releaseType == 2 ? 'auctionDetails.html?id='+v.id : 'artworkDetails.html?id='+v.id}"><div class="artwork-mask-wrap"></div>`;
+							// 	html+=`<img class="bzy-e-list-img" src="`+v.secondPic+`" >`;
+							// }else{
 								html +=  '<li><i onclick="openVideo(' + i + ')"></i>';
 								html +=  `<a class="artwork-mask videoPlay" href="${v.releaseType == 2 ? 'auctionDetails.html?id='+v.id : 'artworkDetails.html?id='+v.id}"><div class="artwork-mask-wrap"></div>`;
 								
-								html+=`<img class="bzy-e-list-img" src="`+v.secondPic+`" >`;
+								html+=`<img class="bzy-e-list-img" src="http://47.118.74.48:8081/`+v.secondPic+`" >`;
 								// html+=`<video x5-video-player-type="h5" x5-video-player-fullscreen="true" x-webkit-airplay="true" webkit-playsinline="true" playsinline="true" style="width:100%;z-index=10" loop="loop" poster="`+v.secondPic+`" src="`+v.primaryPic+`" muted="muted"></video>`;
-							}
+							// }
 						}else{
 							html +=  `<li><a class="artwork-mask" href="${v.releaseType == 2 ? 'auctionDetails.html?id='+v.id : 'artworkDetails.html?id='+v.id}"><div class="artwork-mask-wrap"></div>`;
 						  html+=`<img class="bzy-e-list-img" src="`+(v.secondPic?v.secondPic:v.primaryPic)+`" >`;
@@ -341,6 +341,11 @@ $(function(){
 		})
 		
 	};
+
+	throttle(resizehandler,window);
+	$(window).resize(function(){
+		throttle(resizehandler,window);
+ 	});
 	
 })
 
@@ -353,10 +358,41 @@ function openVideo(e){
 		$(nowData).addClass('openVideo');//添加类名，标识此节点视频是否打开
 		imgVideo[1].remove();//删除img标签
 		$(imgVideo[0]).after(`<video x5-video-player-type="h5" x5-video-player-fullscreen="true" x-webkit-airplay="true" webkit-playsinline="true" playsinline="true" style="width:100%;z-index=10" loop="loop" autoPlay="autoplay" poster="http://47.118.74.48:8081/`+ajaxList[e].secondPic+`" src="http://47.118.74.48:8081/`+ajaxList[e].primaryPic+`" muted="muted"></video>`);//插入节点
+		
+		
 	}else{
 		$(nowData).removeClass('openVideo');//添加类名，标识此节点视频是否打开
 		imgVideo[1].remove();//删除video标签
-		$(imgVideo[0]).after(`<img class="bzy-e-list-img" src="`+ajaxList[e].secondPic+`" >`)
+		$(imgVideo[0]).after(`<img class="bzy-e-list-img" src="http://47.118.74.48:8081/`+ajaxList[e].secondPic+`" >`)
+		$(imgVideo[1]).height($(nowData).width());
 	}
-	console.log(nowData.className);
+	setTimeout(function(){
+		
+		nowData = $('.bzy-e-list').children()[e];//获取当前的li标签
+		nowImgVideo = $(nowData).children()[1];//获取当前a标签
+		imgVideo = $(nowImgVideo).children();//获取a标签下所有子节点
+		$(imgVideo[1]).height($(nowData).width());
+		console.log(imgVideo);
+	})
+}
+// 函数节流
+function throttle(method,context){
+	clearTimeout(method.tId);
+	method.tId=setTimeout(function(){
+			method.call(context);
+	},500);
+}
+
+// 图片尺寸变为正方形
+function resizehandler(){
+	let nowData = $('.bzy-e-list').children();//获取当前的li标签
+	console.log(nowData.length);
+	if(nowData.length > 0){//首次加载dom节点可能未加载
+		for(let i=0;i<nowData.length;i++){
+			let nowImgVideo = $(nowData).children()[i];//获取当前a标签
+			let imgVideo = $(nowImgVideo).children();//获取a标签下所有子节点
+			$(imgVideo[1]).height($(nowData[i]).width());
+			console.log(imgVideo[i]);
+		}
+	}
 }
