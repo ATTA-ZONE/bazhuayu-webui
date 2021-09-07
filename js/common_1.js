d111ff119f9c19864ac8676b5788e5587f276fdb
mouren <1832819123@qq.com>
2021-09-07 16:57:52 +0800

    walletconnect 添加提示

--- js/common_1.js
+++ js/common_1.js
@@ -86,9 +86,7 @@ function RPCSwitchHint(res) {
 	if (res && res != targetChainId && getCookie('isConnect')=='true') {
 		$('.rpcname').text(commonText.tips01+RPCSetting[res]['CHAIN_NAME']+commonText.tips02);
 		$('.target-rpcname').text(RPCSetting[targetChainId]['CHAIN_NAME']);
-		if (getCookie('_wallet_')=='MetaMask' && getCookie('islogin')=='true') {
-			$('.bsc-tips').show()
-		}
+		$('.bsc-tips').show()
 	} else {
 		$('.bsc-tips').hide()
 	}
