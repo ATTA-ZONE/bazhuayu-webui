9c0c624eef2667e3d5b089e5b5532709dbbcf7f4
mouren <1832819123@qq.com>
2021-09-17 16:17:07 +0800

    添加购买面具

--- js/common_1.js
+++ js/common_1.js
@@ -1,8 +1,7 @@
-
 //
 var url = window.location.pathname;
-url = url.substring(url.lastIndexOf('/')+1);
-url = url.substring(0,url.indexOf('.'));
+url = url.substring(url.lastIndexOf('/') + 1);
+url = url.substring(0, url.indexOf('.'));
 //var base_url = 'http://47.118.74.48:8081';
 // var base_url = 'http://58.212.110.92:8866';
 var base_url = '';
@@ -13,154 +12,160 @@ var walletType = getCookie(CHAIN.WALLET.__wallet__)
 
 if (getCookie('islogin') != 'false') {
 	islogin = true;
-}else{
+} else {
 	islogin = false;
 }
 
 if (window.location.href.indexOf('bazhuayu.io') == -1) {
 	base_url = 'http://localhost:8081';
 	if (window.location.href.indexOf('47.118.74.48:') > -1) {
-		base_url = 'http://47.118.74.48:'+window.location.port;
+		base_url = 'http://47.118.74.48:' + window.location.port;
 	}
 	targetChainId = 97;
 }
-var lang = getCookie("lang")?getCookie("lang"):'TC';
+var lang = getCookie("lang") ? getCookie("lang") : 'TC';
 var commonText = chEnText.common[lang];
 $.ajax({
-	url:base_url+'/v2/user/lang/select',
-	type:'POST',
-	dataType:'json',
-	data:{lang:lang},
-	success:function(res){
-	}
+	url: base_url + '/v2/user/lang/select',
+	type: 'POST',
+	dataType: 'json',
+	data: {
+		lang: lang
+	},
+	success: function (res) {}
 });
 
 //询问弹窗
-function logoutConfirm(){
-	hsycms.confirm('confirm',commonText.logOutMes,
-		function(res){            
-			hsycms.success('success',commonText.suc);
-			setTimeout(function(){
+function logoutConfirm() {
+	hsycms.confirm('confirm', commonText.logOutMes,
+		function (res) {
+			hsycms.success('success', commonText.suc);
+			setTimeout(function () {
 				$.ajax({
-					url:base_url+'/logout',
-					type:"POST",
-					dataType:'json',
-					success:function(res){
-						if(res.code==0){
+					url: base_url + '/logout',
+					type: "POST",
+					dataType: 'json',
+					success: function (res) {
+						if (res.code == 0) {
 							// window.location.reload();
 							window.location.href = 'index.html';
 						}
 					}
 				})
-			},1500)
+			}, 1500)
 		},
-		function(){},
+		function () {},
 	)
 };
 
 
-function headIconShow(){
+function headIconShow() {
 	$('.menu-list2').addClass('menu-list-show');
 	$('video').addClass('video-hidden');
 }
-function headIconHide(){
+
+function headIconHide() {
 	$('.menu-list2').removeClass('menu-list-show');
 	$('video').removeClass('video-hidden');
 }
 
-function walletaddressreplace(){
+function walletaddressreplace() {
 	window.location.href = 'connectWallet.html';
 }
-function walletaddressdelete(){
+
+function walletaddressdelete() {
 	$('.modify-btn-active').removeClass('walletaddress-replace');
 	$('.cancel').removeClass('walletaddress-delete');
 	$('.cancel').text('取消');
 	$('.modify-tips').hide();
 	deleteWallet();
 }
+
 function closeBsc() {
 	$('.bsc-tips').hide()
 }
 
 function RPCSwitchHint(res) {
-	if (res && res != targetChainId && getCookie('isConnect')=='true') {
-		$('.rpcname').text(commonText.tips01+RPCSetting[res]['CHAIN_NAME']+commonText.tips02);
+	if (res && res != targetChainId && getCookie('isConnect') == 'true') {
+		$('.rpcname').text(commonText.tips01 + RPCSetting[res]['CHAIN_NAME'] + commonText.tips02);
 		$('.target-rpcname').text(RPCSetting[targetChainId]['CHAIN_NAME']);
-		$('.bsc-tips').show()
+		if (location.pathname != '/mobile/tc/specialTool.html') {
+			$('.bsc-tips').show()
+		}
 	} else {
 		$('.bsc-tips').hide()
 	}
 }
 
 function changenetwork() {
-	CHAIN.WALLET.switchRPCSettings(targetChainId).then(()=>{
+	CHAIN.WALLET.switchRPCSettings(targetChainId).then(() => {
 		$('.bsc-tips').hide()
 	})
 }
 
-$(function(){
+$(function () {
 
 	// 中英文切换
 	$("body").on("click", ".language-change-en", function () {
-		setCookie('lang','EN');
-		window.location.href=window.location.href; 
+		setCookie('lang', 'EN');
+		window.location.href = window.location.href;
 	})
 	$("body").on("click", ".language-change-ch", function () {
-		setCookie('lang','TC');
-		window.location.href=window.location.href;
+		setCookie('lang', 'TC');
+		window.location.href = window.location.href;
 	})
-	
-	if(lang == "TC"){
+
+	if (lang == "TC") {
 		$(".headerpage").load("header.html");
 		$(".footerpage").load("footer.html");
 		$(".tips").load("tips.html");
-	}else{
+	} else {
 		$(".headerpage").load("header2.html");
 		$(".footerpage").load("footer2.html");
 		$(".tips").load("tips2.html");
 	}
-	
+
 	//底部配置信息
 	$.ajax({
-		url:'/sys/concat',
-		success:function(res){
-			var weiboUrl =res.data.weiboUrl;
-			var twitterUrl=res.data.twitterUrl;
-			var telegramUrl=res.data.telegramUrl;
-			var qrPath1=res.data.qrPath;
-			var qrPath = JSON.parse(qrPath1)[0];			
-			$(".weiboUrl").attr("href",weiboUrl);
+		url: '/sys/concat',
+		success: function (res) {
+			var weiboUrl = res.data.weiboUrl;
+			var twitterUrl = res.data.twitterUrl;
+			var telegramUrl = res.data.telegramUrl;
+			var qrPath1 = res.data.qrPath;
+			var qrPath = JSON.parse(qrPath1)[0];
+			$(".weiboUrl").attr("href", weiboUrl);
 			// $(".twitterUrl").attr("href",twitterUrl);
-			$(".telegramUrl").attr("href",telegramUrl);
-			$(".code-pic").attr("src",qrPath);
+			$(".telegramUrl").attr("href", telegramUrl);
+			$(".code-pic").attr("src", qrPath);
 		},
-		
-	})	
-	//
-	$('.bzy-d-head-right input').on('focus',function(){
-		$(this).parent().css('border','1px solid #9567FF');
-	}).on('blur',function(){
-		$(this).parent().css('border','1px solid #141414');
+
 	})
-	
-	
 	//
-	$('.modify-tc-pc').on('click',function(){
+	$('.bzy-d-head-right input').on('focus', function () {
+		$(this).parent().css('border', '1px solid #9567FF');
+	}).on('blur', function () {
+		$(this).parent().css('border', '1px solid #141414');
+	})
+
+
+	//
+	$('.modify-tc-pc').on('click', function () {
 		$('.modify').fadeIn();
 	})
-	
+
 	//
 	var mobile_width = $(window).width();
-	if(mobile_width<=992){
+	if (mobile_width <= 992) {
 		$('.tc-show').removeClass('modify-tc-pc');
 		$('.tc-show').addClass('modify-tc-mobile');
-		
-		$('.accountclass.modify-tc-mobile').on('click',function(){
+
+		$('.accountclass.modify-tc-mobile').on('click', function () {
 			$('.modify').addClass('modify-tc-active')
 		})
 	}
 
-	$('body').append('<div class="bsc-tips" style="display:none;position:absolute;top:80px;left:50%;transform:translateX(-50%);z-index:9999;color:#fff;background: #9567FF;border-radius: 10px;white-space: nowrap;padding:10px 20px;"><span class="rpcname">'+commonText.tips01+commonText.tips02+'</span><a onclick="changenetwork()" class="target-rpcname">'+commonText.tips02+'</a><img onclick="closeBsc()" style="width: 20px;vertical-align: bottom;" src="./images/Close.png" /></div>')
+	$('body').append('<div class="bsc-tips" style="display:none;position:absolute;top:80px;left:50%;transform:translateX(-50%);z-index:9999;color:#fff;background: #9567FF;border-radius: 10px;white-space: nowrap;padding:10px 20px;"><span class="rpcname">' + commonText.tips01 + commonText.tips02 + '</span><a onclick="changenetwork()" class="target-rpcname">' + commonText.tips02 + '</a><img onclick="closeBsc()" style="width: 20px;vertical-align: bottom;" src="./images/Close.png" /></div>')
 	$('.bsc-tips').hide()
 	if (islogin) {
 		CHAIN.WALLET.chainId()
@@ -168,33 +173,33 @@ $(function(){
 				RPCSwitchHint(res)
 			})
 	}
-	
-	
+
+
 	// 用户信息
 	$.ajax({
-		url:base_url+'/v2/user/account',
-		success:function(res){
+		url: base_url + '/v2/user/account',
+		success: function (res) {
 			var html = ``;
-			if(res.code==0){
-				
+			if (res.code == 0) {
+
 				html += `<a class="header-right-yidl" href="javascript:void(0);">`;
 				html += `	<div class="header-right-yidl-info flex">`;
-				if(res.data.headIcon==null||res.data.headIcon==''){
+				if (res.data.headIcon == null || res.data.headIcon == '') {
 					html += `	<div><img src="./images/Ellipse 93.png" ></div>
 								<p>
-									<span class="ellipsis">`+res.data.name+`</span>
-									<span class="my-email">`+res.data.email+`</span>
+									<span class="ellipsis">` + res.data.name + `</span>
+									<span class="my-email">` + res.data.email + `</span>
 								</p>`;
-				}else{
-					html += `	<div><img src="`+base_url+res.data.headIcon+`" ></div>
+				} else {
+					html += `	<div><img src="` + base_url + res.data.headIcon + `" ></div>
 								<p>
-									<span class="ellipsis">`+res.data.name+`</span>
-									<span class="my-email">`+res.data.email+`</span>
+									<span class="ellipsis">` + res.data.name + `</span>
+									<span class="my-email">` + res.data.email + `</span>
 								</p>`;
-					$('.mobile-head-icon img').attr('src',res.data.headIcon);
+					$('.mobile-head-icon img').attr('src', res.data.headIcon);
 				};
 				html += `	</div>`;
-				
+
 				html += `	<div class="header-right-yidl-my none">
 								
 								<span onclick="window.location.href = 'myaccount.html'">${commonText.myaccount}</span>
@@ -203,52 +208,41 @@ $(function(){
 								<span onclick="window.location.href = 'mywallet.html'">${commonText.mywallet}</span>
 								<span class="logout" onclick="logoutConfirm()">${commonText.logOut}</span>
 							</div>`;
-				
+
 				html += `</a>`;
-				
+
 				$('.menu-list-con-meail').text(res.data.email);
-				// $('.menu-list-con-meail').show();
-				// $('.mobile-ydl').show();
-				// $('.mobile-login').hide();
-				// $('.mobile-logout').show();
 				var mobile_width = $(window).width();
-				if(mobile_width<=992){
+				if (mobile_width <= 992) {
 					$('.mobile-head-icon').show();
 					$('.mobile-login').hide();
 				}
-			}else{
+			} else {
 				$('.mobile-head-icon').hide();
 				$('.mobile-login').show();
-				// $('.header-right-yidl').hide();
-				// $('.header-right-login').show();
 				html += `<a class="header-right-login flex" href="login.html">${commonText.login}</a>`;
-				
-				// $('.menu-list-con-meail').hide();
-				// $('.mobile-ydl').hide();
-				// $('.mobile-login').show();
-				// $('.mobile-logout').hide();
 			}
 			$('.header-dl').html(html);
 		}
 	});
 	$.ajax({
-		url:base_url + "/v2/commodity/channel/list",
-		success : function (res) {
-			if (res.code==0) {
+		url: base_url + "/v2/commodity/channel/list",
+		success: function (res) {
+			if (res.code == 0) {
 				let data = res.data.types;
 				let html = `<li class="${url == 'index' ? 'current' : ''}"><a class="language-tc" href="index.html">${commonText.home}</a></li>`;
 				html += `<li class="${url == 'blindbox' ? 'current' : ''}"><a class="language-tc" href="blindbox.html">${commonText.blindbox}</a></li>`;
 				data.forEach(item => {
-					html += `<li class="${window.location.search == '?id='+item.id ? 'current' : ''}"><a class="language-tc" href="artwork.html?id=`+item.id+`">${item.name}</a></li>`
+					html += `<li class="${window.location.search == '?id='+item.id ? 'current' : ''}"><a class="language-tc" href="artwork.html?id=` + item.id + `">${item.name}</a></li>`
 				});
 				html += `<li class="${url == 'auctionDetails' ? 'current' : ''}"><a class="language-tc" href="auctionDetails.html?id=5">${commonText.auction}</a></li>`;
 				html += `<li class="${url == 'specialTool' ? 'current' : ''}"><a class="language-tc" href="specialTool.html">${commonText.specialTool}</a></li>`;
 				$('.nav-header').html(html);
-				
+
 				let html_h5 = `<li><a class="language-tc" href="index.html">${commonText.home}</a></li>`;
 				html_h5 += `<li><a class="language-tc" href="blindbox.html">${commonText.blindbox}</a></li>`;
 				data.forEach(item => {
-					html_h5 += `<li><a class="language-tc" href="artwork.html?id=`+item.id+`">${item.name}</a></li>`;
+					html_h5 += `<li><a class="language-tc" href="artwork.html?id=` + item.id + `">${item.name}</a></li>`;
 				});
 				html_h5 += `<li class="mobile-connect-wallet"><a class="language-tc" onclick="connectWallet()" href="javascript:void(0);">${commonText.noConnectWallet}</a></li>
 							<li class="switchlanguage_mobile">
@@ -266,13 +260,13 @@ $(function(){
 	// 百度统计代码
 	var hm = document.createElement("script");
 	hm.src = "https://hm.baidu.com/hm.js?9bd005a002797de8867dca5fca16ee90";
-	var s = document.getElementsByTagName("script")[0]; 
+	var s = document.getElementsByTagName("script")[0];
 	s.parentNode.insertBefore(hm, s);
 })
 
 function updateWalletStatus() {
-	$('.header-right-wallet').html('<img src="./images/point-loading.gif" style="width:6px; margin-right:5px;"><span>'+commonText.wallet+'</span>');
-	$('.mobile-connect-wallet').html('<img src="./images/point-loading.gif" style="width:6px; margin-right:5px; "/><a class="language-tc" style="width:calc(100% - 11px)" onclick="connectWallet()" href="javascript:void(0);">'+commonText.wallet+'</a>');
+	$('.header-right-wallet').html('<img src="./images/point-loading.gif" style="width:6px; margin-right:5px;"><span>' + commonText.wallet + '</span>');
+	$('.mobile-connect-wallet').html('<img src="./images/point-loading.gif" style="width:6px; margin-right:5px; "/><a class="language-tc" style="width:calc(100% - 11px)" onclick="connectWallet()" href="javascript:void(0);">' + commonText.wallet + '</a>');
 	$('.header-right-wallet').show();
 	$('.mobile-connect-wallet').show();
 
@@ -280,14 +274,14 @@ function updateWalletStatus() {
 	if (islogin) {
 		$('.header-right-wallet').show();
 		$.ajax({
-			url:base_url+'/v2/user/wallet/info',
-			success:function(res){
-				if(res.code==0){
+			url: base_url + '/v2/user/wallet/info',
+			success: function (res) {
+				if (res.code == 0) {
 					walletId = res.data.address;
 					walletType = res.data.walletType || 'MetaMask'
-					setCookie('_wallet_',walletType);
+					setCookie('_wallet_', walletType);
 					CHAIN.WALLET.accounts()
-						.then(function(account){
+						.then(function (account) {
 							setCookie('isConnect', false);
 							if (walletId && account.length) {
 								if (walletId == account[0]) {
@@ -303,17 +297,17 @@ function updateWalletStatus() {
 								displayWalletStatus(2, account);
 							}
 						})
-				} else if (res.code==1002 && islogin) {
-					setCookie('islogin',false);
+				} else if (res.code == 1002 && islogin) {
+					setCookie('islogin', false);
 					window.location.href = 'index.html';
-				}else{
+				} else {
 					$('.header-right-wallet').hide();
 					$('.mobile-connect-wallet').hide();
-					setCookie('isConnect',false);
+					setCookie('isConnect', false);
 				}
 			}
 		})
-	}else{
+	} else {
 		$('.header-right-wallet').hide();
 	}
 }
@@ -322,40 +316,39 @@ if (islogin) {
 	CHAIN.WALLET.networkChangedAssign(RPCSwitchHint);
 }
 
-function showwalletaddress(e){
+function showwalletaddress(e) {
 	if ($('.header-right-wallet .modify-tc-pc').text() == commonText.noConnectWallet || $('.mobile-connect-wallet .modify-tc-pc').text() == commonText.noConnectWallet) {
 		connectWallet();
-	}else{
-		window.location.href  = 'showwallet.html';
+	} else {
+		window.location.href = 'showwallet.html';
 	}
 }
 
-function displayWalletStatus(status, account){
-	if (status==0) {
+function displayWalletStatus(status, account) {
+	if (status == 0) {
 		// 钱包与 绑定钱包 相同
-		setCookie('isConnect',true);
-		$('.header-right-wallet').html('<img src="./images/point.png" style="width:6px; margin-right:5px;"><span class="modify-tc-pc tc-show">'+commonText.connectWallet+'</span><p class="walletIdshow">'+ account[0] +'</p>');
-		$('.mobile-connect-wallet').html('<img src="./images/point.png" style="width:6px; margin-right:5px; "/><a class="language-tc modify-tc-pc tc-show" style="width:calc(100% - 11px)" href="javascript:void(0);">'+commonText.connectWallet+'</a><p class="walletIdshow">'+ account[0] +'</p>');		
-			$('.mobile-connect-wallet').html('<img src="./images/point.png" style="width:6px; margin-right:5px; "/><a class="language-tc modify-tc-pc tc-show" style="width:calc(100% - 11px)" href="javascript:void(0);">'+commonText.connectWallet+'</a><p class="walletIdshow">'+ account[0] +'</p>');
-		$('.mobile-connect-wallet').html('<img src="./images/point.png" style="width:6px; margin-right:5px; "/><a class="language-tc modify-tc-pc tc-show" style="width:calc(100% - 11px)" href="javascript:void(0);">'+commonText.connectWallet+'</a><p class="walletIdshow">'+ account[0] +'</p>');		
-		$('.mobile-connect-wallet,.header-right-wallet').click(function(){
+		setCookie('isConnect', true);
+		$('.header-right-wallet').html('<img src="./images/point.png" style="width:6px; margin-right:5px;"><span class="modify-tc-pc tc-show">' + commonText.connectWallet + '</span><p class="walletIdshow">' + account[0] + '</p>');
+		$('.mobile-connect-wallet').html('<img src="./images/point.png" style="width:6px; margin-right:5px; "/><a class="language-tc modify-tc-pc tc-show" style="width:calc(100% - 11px)" href="javascript:void(0);">' + commonText.connectWallet + '</a><p class="walletIdshow">' + account[0] + '</p>');
+		$('.mobile-connect-wallet').html('<img src="./images/point.png" style="width:6px; margin-right:5px; "/><a class="language-tc modify-tc-pc tc-show" style="width:calc(100% - 11px)" href="javascript:void(0);">' + commonText.connectWallet + '</a><p class="walletIdshow">' + account[0] + '</p>');
+		$('.mobile-connect-wallet').html('<img src="./images/point.png" style="width:6px; margin-right:5px; "/><a class="language-tc modify-tc-pc tc-show" style="width:calc(100% - 11px)" href="javascript:void(0);">' + commonText.connectWallet + '</a><p class="walletIdshow">' + account[0] + '</p>');
+		$('.mobile-connect-wallet,.header-right-wallet').click(function () {
 			showwalletaddress();
 		});
-	} else if (status==1) {
+	} else if (status == 1) {
 		// 钱包与 绑定钱包 不同/未绑定
 		//setCookie('isConnect',true);
-		$('.header-right-wallet').html('<img src="./images/point-yellow.png" style="width:6px; margin-right:5px;"><span class="modify-tc-pc tc-show">'+commonText.unBind+'</span><p class="walletIdshow">'+ account[0] +'</p>');
-		$('.mobile-connect-wallet').html('<img src="./images/point-yellow.png" style="width:6px; margin-right:5px; "/><a class="language-tc modify-tc-pc tc-show" style="width:calc(100% - 11px)" href="javascript:void(0);">'+commonText.unBind+'</a><p class="walletIdshow">'+ account[0] +'</p>');
-		$('.mobile-connect-wallet,.header-right-wallet').click(function(){
+		$('.header-right-wallet').html('<img src="./images/point-yellow.png" style="width:6px; margin-right:5px;"><span class="modify-tc-pc tc-show">' + commonText.unBind + '</span><p class="walletIdshow">' + account[0] + '</p>');
+		$('.mobile-connect-wallet').html('<img src="./images/point-yellow.png" style="width:6px; margin-right:5px; "/><a class="language-tc modify-tc-pc tc-show" style="width:calc(100% - 11px)" href="javascript:void(0);">' + commonText.unBind + '</a><p class="walletIdshow">' + account[0] + '</p>');
+		$('.mobile-connect-wallet,.header-right-wallet').click(function () {
 			showwalletaddress();
 		});
-	} else if (status==2) {
+	} else if (status == 2) {
 		// 钱包 未授权
-		$('.header-right-wallet').html('<img src="./images/point-red.png" style="width:6px; margin-right:5px;"><span class="modify-tc-pc tc-show">'+commonText.noConnectWallet+'</span>');
-		$('.mobile-connect-wallet').html('<img src="./images/point-red.png" style="width:6px; margin-right:5px; "/><a class="language-tc modify-tc-pc tc-show" style="width:calc(100% - 11px)" href="javascript:void(0);">'+commonText.noConnectWallet+'</a>');
-		$('.mobile-connect-wallet,.header-right-wallet').click(function(){
+		$('.header-right-wallet').html('<img src="./images/point-red.png" style="width:6px; margin-right:5px;"><span class="modify-tc-pc tc-show">' + commonText.noConnectWallet + '</span>');
+		$('.mobile-connect-wallet').html('<img src="./images/point-red.png" style="width:6px; margin-right:5px; "/><a class="language-tc modify-tc-pc tc-show" style="width:calc(100% - 11px)" href="javascript:void(0);">' + commonText.noConnectWallet + '</a>');
+		$('.mobile-connect-wallet,.header-right-wallet').click(function () {
 			showwalletaddress();
 		});
 	}
 }
-
--- js/specialTool.js
+++ js/specialTool.js
@@ -17,7 +17,7 @@ var app = new Vue({
 			auctionAddress: '',
 			auctionContractInstance: null,
 			userAddress: '',
-			chainId: '',
+			chainId: 0,
 			// 中英文切换
 			languageType: "",
 			chEnTextHtml: {
@@ -138,6 +138,7 @@ var app = new Vue({
 		self.getComditInfo()
 		self.initAddress()
 	},
+
 	mounted() {
 		$('.payment-page-right-crypto').show();
 		if (getCookie('isConnect') != 'true') {
@@ -151,6 +152,9 @@ var app = new Vue({
 		$('.order-price .order-price-busd').show();
 	},
 	methods: {
+		saleStatus(startTime, endTime) {
+			return startTime < Date.now() && endTime > Date.now()
+		},
 		payCrypto() {
 			let self = this
 			if ($('#cryptoBtn').text() == '去我的資產核對' || $('#cryptoBtn').text() == 'Go to my asset to check') {
@@ -164,7 +168,6 @@ var app = new Vue({
 			if ($('#cryptoBtn').text() == '立即付款  ->' || $('#cryptoBtn').text() == 'Pay now  ->') {
 				CHAIN.WALLET.accounts().then(function (accounts) {
 					self.getOnSellToken(accounts);
-					loading();
 				});
 			}
 		},
@@ -183,16 +186,7 @@ var app = new Vue({
 				})
 			CHAIN.WALLET.chainId()
 				.then(function (res) {
-					let id = ''
 					self.chainId = web3.utils.hexToNumber(res);
-					id = web3.utils.hexToNumber(res);
-					if (id == self.targetChainId) {
-						self.auctionAddress = contractSetting['vending_machine'][id].address; //网络切换
-					} else {
-						tips(self.chEnTextHtml[self.languageType].switchNet)
-					}
-					var auctionABI = contractSetting['vending_machine']['abi'];
-					self.auctionContractInstance = new web3.eth.Contract(auctionABI, self.auctionAddress);
 				})
 		},
 		saveHash(id, hash) {
@@ -209,64 +203,124 @@ var app = new Vue({
 				}),
 			});
 		},
+		getDrawResult(hash) {
+			$.ajax({
+				url: base_url + "/v2/activity/getDrawResult",
+				data: {txhash: hash || ""}
+			});
+		},
 		getOnSellToken(accounts) {
-			let self = this;
 			if (accounts.length < 1) {
 				return false;
 			}
 			var cwallet = ""; //收款钱包 地址
 
-			if (self.targetChainId == 97) {
+			if (this.targetChainId == 97) {
 				cwallet = "0x5ea57A85e0f3C9085e13597a35BFd6a82Bbf7127";
 			} else {
 				cwallet = "0xC6F6fCce3026f08C668cA09bc5dFB58e596520f4";
 			}
-			var web3 = new Web3(CHAIN.WALLET.provider());
+			if (this.saleItem.id == 70) {
+				this.buyEth(accounts)
+			} else {
+				this.buyBusd(cwallet, accounts)
+			}
 
-			var chainId = "";
-			CHAIN.WALLET.chainId().then(function (res) {
-				chainId = web3.utils.hexToNumber(res);
-				// busdAddress 供外界使用
-				var busdAddress = contractSetting["busd_ERC20"][chainId].address;
-				var busdABI = contractSetting["busd_ERC20"]["abi"];
-				busdContractInstance = new web3.eth.Contract(busdABI, busdAddress);
-				var amount = $("#specialTool .payment .busdPrice")
-					.text()
-					.split("BUSD ")[1];
-				var num = web3.utils.toWei(amount, "ether");
-				busdContractInstance.methods
-					.balanceOf(accounts[0])
-					.call() //查询余额
-					.then(function (res2) {
-						if (Number(res2) >= Number(num)) {
-							setTimeout(function () {
-								busdContractInstance.methods
-									.transfer(cwallet, num)
-									.send({
-										//转账
-										from: accounts[0],
-									})
-									.on("transactionHash", function (hash) {
-										self.saveHash(self.saleItem.id, hash);
-									})
-									.then(() => {
-										loading(self.chEnTextHtml[self.languageType].purchaseSuc)
-										$('#cryptoBtn').html(self.chEnTextHtml[self.languageType].asset)
-										loadingHide();
-										self.selectarr = [1]
-									})
-									.catch((err) => {
-										console.log(err);
-										loadingHide();
-										self.selectarr = [1]
-									});
-							}, 1000);
-						} else {
-							tips(self.chEnTextHtml[self.languageType].balanceInsufficient);
-						}
-					});
-			});
 		},
+		buyEth(accounts) {
+			let self = this;
+			if (self.chainId != 1 && self.chainId != 4) {
+				tips(self.chEnTextHtml[self.languageType].switchNet)
+				return false
+			}
+			loading();
+			var web3 = new Web3(CHAIN.WALLET.provider());
+			var busdAddress = contractSetting["eth_ERC20"][self.chainId].address;
+			var busdABI = contractSetting["eth_ERC20"]["abi"];
+			busdContractInstance = new web3.eth.Contract(busdABI, busdAddress);
+			var amount = $("#specialTool .payment .busdPrice")
+				.text()
+				.split("ETH ")[1];
+			var num = web3.utils.toWei(amount, "ether");
+			busdContractInstance.methods
+				.draw(self.selectarr.length)
+				.send({
+					//转账
+					from: accounts[0],
+					value: num
+				})
+				.on("transactionHash", function (hash) {
+					tips(self.chEnTextHtml[self.languageType].seconds)
+					loading()
+					//self.saveHash(self.saleItem.id, hash);
+				})
+				.then((result) => {
+					loadingHide();
+					self.getDrawResult(result.blockHash)
+					tips(self.chEnTextHtml[self.languageType].purchaseSuc)
+					$('#cryptoBtn').html(self.chEnTextHtml[self.languageType].asset)
+					loadingHide();
+					self.selectarr = [1]
+				})
+				.catch((err) => {
+					console.log(err);
+					loadingHide();
+					self.selectarr = [1]
+				});
+		},
+
+		buyBusd(cwallet, accounts) {
+			let self = this;
+			if (self.chainId != 56 && self.chainId != 97) {
+				tips(self.chEnTextHtml[self.languageType].switchNet)
+				return false
+			}
+			loading();
+			var web3 = new Web3(CHAIN.WALLET.provider());
+			var busdAddress = contractSetting["busd_ERC20"][self.chainId].address;
+			var busdABI = contractSetting["busd_ERC20"]["abi"];
+			busdContractInstance = new web3.eth.Contract(busdABI, busdAddress);
+			var amount = $("#specialTool .payment .busdPrice")
+				.text()
+				.split("BUSD ")[1];
+			var num = web3.utils.toWei(amount, "ether");
+			busdContractInstance.methods
+				.balanceOf(accounts[0])
+				.call() //查询余额
+				.then(function (res2) {
+					if (Number(res2) >= Number(num)) {
+						loadingHide();
+						setTimeout(function () {
+							busdContractInstance.methods
+								.transfer(cwallet, num)
+								.send({
+									//转账
+									from: accounts[0],
+								})
+								.on("transactionHash", function (hash) {
+									tips(self.chEnTextHtml[self.languageType].seconds)
+									loading()
+									self.saveHash(self.saleItem.id, hash);
+								})
+								.then(() => {
+									loadingHide();
+									tips(self.chEnTextHtml[self.languageType].purchaseSuc)
+									$('#cryptoBtn').html(self.chEnTextHtml[self.languageType].asset)
+									loadingHide();
+									self.selectarr = [1]
+								})
+								.catch((err) => {
+									console.log(err);
+									loadingHide();
+									self.selectarr = [1]
+								});
+						}, 1000);
+					} else {
+						tips(self.chEnTextHtml[self.languageType].balanceInsufficient);
+					}
+				});
+		},
+
 		getComditInfo() {
 			//商品详情业加载
 			let self = this
@@ -285,6 +339,20 @@ var app = new Vue({
 					}
 				}
 			})
+		},
+
+		timeFormat(str) {
+			var date = new Date(str);
+			Y = date.getFullYear() + "-";
+			M =
+				(date.getMonth() + 1 < 10 ?
+					"0" + (date.getMonth() + 1) :
+					date.getMonth() + 1) + "-";
+			D = date.getDate() + " ";
+			h = date.getHours() + ":";
+			m = date.getMinutes() + ":";
+			s = date.getSeconds();
+			return Y + M + D + h + m + s;
 		},
 
 		toggleVideo() {
